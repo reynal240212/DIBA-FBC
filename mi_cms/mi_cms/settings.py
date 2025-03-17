39c1260694b8784ee39709ab887a7358c4bc9ac8
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-03-17 04:38:11 +0000

    se crea core

--- mi_cms/mi_cms/settings.py
+++ mi_cms/mi_cms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "core",  # Aplicación "core" creada en el paso anterior
 ]
 
 MIDDLEWARE = [
