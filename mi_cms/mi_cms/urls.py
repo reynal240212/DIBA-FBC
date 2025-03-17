4c9c8737b8f5e654fac5ce249a47fd12d3c17ddd
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-03-17 04:41:34 +0000

    actualizacion

--- mi_cms/mi_cms/urls.py
+++ mi_cms/mi_cms/urls.py
@@ -1,29 +1,6 @@
-"""
-URL configuration for mi_cms project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
-from django.contrib import admin
-from django.urls import path
-from core import views  # Importa las vistas de la app "core"
+from core import views  # Importamos las vistas de la app "core"
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # Panel de administración de Django
@@ -32,3 +9,15 @@ urlpatterns = [
     path('partidos/', views.partidos, name='partidos'),  # Sección de partidos
     path('contacto/', views.contacto, name='contacto'),  # Página de contacto
 ]
+#
+# # Internationalization
+# # https://docs.djangoproject.com/en/5.1/topics/i18n/
+#
+# LANGUAGE_CODE = "en-us"
+#
+# TIME_ZONE = "UTC"
+#
+# USE_I18N = True
+#
+# USE_L10N = True
+# 
