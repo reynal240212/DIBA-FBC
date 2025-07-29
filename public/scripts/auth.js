6eabc127a7c9edd3f14769bd055b3790570c1d66
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-07-29 15:50:16 -0500

    .

---
+++ public/scripts/auth.js
@@ -0,0 +1,6 @@
+// js/auth.js
+
+const SUPABASE_URL = 'https://wdnlqfiwuocmmcdowjyw.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbmxxZml3dW9jbW1jZG93anl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjY1ODAsImV4cCI6MjA2NDEwMjU4MH0.4SCS_NRDIYLQJ1XouqW111BxkMOlwMWOjje9gFTgW_Q';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
