e37b65af9a7ebe3cacd6c457ea33f958e5d686f3
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-07-29 15:55:12 -0500

    .

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -1,6 +1,20 @@
-// js/auth.js
+// auth.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = 'https://wdnlqfiwuocmmcdowjyw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbmxxZml3dW9jbW1jZG93anl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjY1ODAsImV4cCI6MjA2NDEwMjU4MH0.4SCS_NRDIYLQJ1XouqW111BxkMOlwMWOjje9gFTgW_Q';
+const supabase = createClient(
+  "https://wdnlqfiwuocmmcdowjyw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbmxxZml3dW9jbW1jZG93anl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjY1ODAsImV4cCI6MjA2NDEwMjU4MH0.4SCS_NRDIYLQJ1XouqW111BxkMOlwMWOjje9gFTgW_Q"
+);
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export async function verificarSesion() {
+  const { data: { session } } = await supabase.auth.getSession();
+  if (!session) {
+    alert("⚠️ Debes iniciar sesión.");
+    window.location.href = "login.html";
+  }
+}
+
+export async function cerrarSesion() {
+  const { error } = await supabase.auth.signOut();
+  if (!error) window.location.href = "login.html";
+}
