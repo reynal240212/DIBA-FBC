84555d05ec534b2a5b3cec9019d5c0750cbc44d8
reynal240212 <124523746+reynal240212@users.noreply.github.com>
2025-09-05 11:23:34 -0500

    .

--- public/scripts/gestor.js
+++ public/scripts/gestor.js
@@ -1,7 +1,6 @@
 // Reemplaza con tus propias claves de Supabase
 const SUPABASE_URL = "https://wdnlqfiwuocmmcdowjyw.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbmxxZml3dW9jbW1jZG93anl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjY1ODAsImV4cCI6MjA2NDEwMjU4MH0.4SCS_NRDIYLQJ1XouqW111BxkMOlwMWOjje9gFTgW_Q";
+const SUPABASE_ANON_KEY ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkbmxxZml3dW9jbW1jZG93anl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MjY1ODAsImV4cCI6MjA2NDEwMjU4MH0.4SCS_NRDIYLQJ1XouqW111BxkMOlwMWOjje9gFTgW_Q";
 
 // Inicializa el cliente de Supabase
 const supabase = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
